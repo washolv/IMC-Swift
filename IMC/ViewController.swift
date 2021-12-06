844f02ecac0d0af8c427dc43533de0a2ea03e2e2
washolv <w_olv@hotmail.com>
2021-12-06 13:29:58 -0300

    App calculate IMC

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var imResult: UIImageView!
+    @IBOutlet weak var lbResult: UILabel!
+    var imc: Double = 0;
+    @IBOutlet weak var viResult: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func calculate(_ sender: Any) {
+        if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!){
+            imc = weight / pow(height,2);
+            showResults();
+        }
+            
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true);
+    }
+    func showResults(){
+        var result: String="";
+        var image: String="";
+        switch imc{
+            case 0..<16:
+                result = "Magreza";
+                image = "magreza"
+            case 16..<18.5:
+                result = "Abaixo do peso";
+                image = "abaixo"
+            case 18.5..<25:
+                result = "Peso Ideal";
+                image = "ideal"
+            case 25..<30:
+                result = "Sobrepeso";
+                image = "sobre"
+            default:
+                result="obesidade";
+                image="obesidade";
+        }
+        lbResult.text="\(Int(imc)): \(result)"
+        imResult.image=UIImage(named: image);
+        viResult.isHidden=false;
+        view.endEditing(true);
+    }
 
 }
 
